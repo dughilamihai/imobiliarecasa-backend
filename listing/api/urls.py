ca32fd9207e4dda4b450e5458e7e7d5c3b41c9a9
dughilamihai <dughilamihai@gmail.com>
2024-11-03 02:20:25 +0200

    added mysql db

--- listing/api/urls.py
+++ listing/api/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'api'
 
 urlpatterns = [
-    path('listings/', views.listing_list, name='listing_list'),
-    path('listings/<int:id>/', views.listing_detail, name='listing_detail'),
+    # path('listings/', views.listing_list, name='listing_list'),
+    # path('listings/<int:id>/', views.listing_detail, name='listing_detail'),
 ]
--- listing/listing/settings.py
+++ listing/listing/settings.py
@@ -77,10 +77,19 @@ WSGI_APPLICATION = 'listing.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'imobiliarecasa',
+        'USER': 'mike',
+        'PASSWORD': 'miti0922',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',  # Setează charset-ul
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
+        },
     }
 }
+
 
 
 # Password validation
